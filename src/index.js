2f36d0559dbd599feb0e91a8eba725617e46f6a8
imtripleq <flamework89@hotmail.com>
2021-10-31 16:18:29 +1100

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  /////// Task List Class
+  const taskList = new TaskList();
+
+  /////// Assign form and button
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDes = document.getElementById("new-task-description");
+  const newTaskPriority = document.getElementById("new-task-priority");
+  const list = document.getElementById("list");
+  const taskUl = document.getElementById("tasks");
+  const renderApp = () => (taskUl.innerHTML = taskList.renderTasks());
+
+  /////optional
+  // const userName = document.getElementById("user-form");
+  const userNameType = document.getElementById("user-form-type");
+
+  ////////////////
+
+  newTaskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    taskList.createNewTask(
+      "Username: " + userNameType.value + "\nDescription: " + newTaskDes.value
+    );
+
+    e.target.reset();
+    renderApp();
+  });
+
+  taskUl.addEventListener("click", (e) => {
+    if (e.target.nodeName === "BUTTON") {
+      taskList.deleteTask(e.target.dataset.description);
+      renderApp();
+    }
+  });
 });
